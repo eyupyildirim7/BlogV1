d29496ee19072dc57d1b8bd7b060f66f76dba336
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2025-03-12 03:07:27 +0300

    Admin page

---
+++ BlogV1/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using BlogV1.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogV1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly BlogDbContext _context;
+
+        public AdminController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Blogs() {
+            var blogs=_context.Blogs.ToList();
+        return View(blogs);
+        }
+    }
+}
